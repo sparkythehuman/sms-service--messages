e0b7b7635dbbe85e13b02919d024e9193634fae4
Joshua T Garcia <garcia.sparky@gmail.com>
2020-11-10 23:25:49 -0700

    fixes misc issues, tested manually

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 import os
 from boto3.dynamodb.conditions import And, Attr, Key
 from uuid import uuid4
@@ -8,8 +9,9 @@ from pytz import timezone
 
 message = boto3.resource('dynamodb').Table('message')
 
-def _get_username(context):
-    return context['authorizer']['claims']['cognito:username']
+
+def _get_username(event):
+    return event['requestContext']['authorizer']['claims']['cognito:username']
 
 
 def _find(username, **kwargs):
@@ -32,41 +34,46 @@ def _find(username, **kwargs):
 def _response(scheduled_messages):
     return {
         'statusCode': 200,
-        'scheduled_messages': scheduled_messages
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': json.dumps(scheduled_messages)
     }
 
 
-def _get(event, context):
+def _get(request, username):
     scheduled_messages = _find(
-        username=_get_username(context)
+        username=username
     )
 
     return _response(scheduled_messages)
 
 
-def _create(event, context):
+def _create(request, username):
     message.put_item(Item={
         'id': 'SMS' + str(uuid4().int)[0:16],
-        'contact_list_id': event['contact-list-id'],
-        'send_at': event['send-at'], # TODO: convert to America/Denver if in other TZ
-        'timezone': event['timezone'],
-        'message': event['message'],
+        'contact_list_id': request['contact_list_id'],
+        # TODO: we may have to concat a date, time, and timezone
+        'send_at': request['send_at'],
+        'message': request['message'],
         'status': 'queued',
+        'username': username,
         'created_at': datetime.now(tz=timezone('America/Denver')).isoformat(),
         'updated_at': datetime.now(tz=timezone('America/Denver')).isoformat()
     })
         
-    return _get(event, context) 
+    return _get(request, username) 
 
 
 def handle(event, context):
-    operation = context['httpMethod']
+    operation = event['requestContext']['httpMethod']
     operations = {
         'GET' : _get,
         'POST': _create
     }
     if operation in operations:
-        return operations[operation](event, context)
+        return operations[operation](json.loads(event['body']), _get_username(event))
     else:
         raise ValueError(f'Unable to run operation for HTTP METHOD: {operation}')
     
